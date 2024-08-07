f26b1cf5fb3929171262edc69258355e739b28e2
joaovdamaceno <damacenovitorj@gmail.com>
2024-08-07 19:51:44 -0300

    mobile update

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
       <div className="text-2xl font-bold">
         meu portfólio 2024
       </div>
-      <div className="absolute left-1/2 transform -translate-x-1/2 flex space-x-3 top-60 md:top-0">
+      <div className="absolute left-1/2 transform -translate-x-1/2 flex space-x-3 top-60 md:top-10">
         <div className="w-2 h-2 bg-black rounded-full"></div>
         <div className="w-2 h-2 bg-red-500 rounded-full"></div>
         <div className="w-2 h-2 bg-gray-800 rounded-full"></div>
